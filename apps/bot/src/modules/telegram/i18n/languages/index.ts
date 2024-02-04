c2b1b5a4b5a820657ee00722309ce011cd6f96eb
nonamich <dimaeromin97@gmail.com>
2024-02-04 14:17:25 +0200

    init i18n

--- apps/bot/src/modules/telegram/i18n/languages/index.ts
+++ apps/bot/src/modules/telegram/i18n/languages/index.ts
@@ -1,11 +1,11 @@
-import ar from './ar.json';
-import en from './en.json';
-import es from './es.json';
-import hi from './hi.json';
-import id from './id.json';
-import pt from './pt.json';
-import tr from './tr.json';
-import uk from './uk.json';
-import vi from './vi.json';
+import ar from './ar/default.json';
+import en from './en/default.json';
+import es from './es/default.json';
+import hi from './hi/default.json';
+import id from './id/default.json';
+import pt from './pt/default.json';
+import tr from './tr/default.json';
+import uk from './uk/default.json';
+import vi from './vi/default.json';
 
 export { ar, en, es, hi, id, pt, tr, uk, vi };

ab10179b024ff116d9f332d701aed043027657cf
nonamich <dimaeromin97@gmail.com>
2024-06-15 10:10:08 +0300

    increase TELEGRAM_MAX_IMAGE_SIZE

--- apps/bot/src/modules/telegram/telegram.utils.ts
+++ apps/bot/src/modules/telegram/telegram.utils.ts
@@ -8,6 +8,9 @@ import {
   TELEGRAM_MIN_IMAGE_SIZE,
 } from './telegram.constants';
 
+const BLACK_LIST_OF_IMAGE =
+  /Путін|Путин|Putin|Flag_of_Russia|Flag_of_the_Soviet|Russian_Imperial_Army|Flag_of_Germany_\(1935–1945\)/i;
+
 export abstract class TelegramUtils {
   static getArticleImage(article: WikiArticle) {
     const images = [article.originalimage, article.thumbnail].filter(
@@ -19,7 +22,7 @@ export abstract class TelegramUtils {
 
   static isValidImage(image: WikiImage) {
     return (
-      !/Flag_of_Russia/.test(image.source) &&
+      !BLACK_LIST_OF_IMAGE.test(image.source) &&
       Utils.isBetween(
         image.width,
         TELEGRAM_MIN_IMAGE_SIZE,

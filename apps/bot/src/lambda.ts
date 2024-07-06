1311013ad7e0714bfb49a15814a8be211e96856f
nonamich <dimaeromin97@gmail.com>
2024-07-06 08:21:28 +0300

    add sentry

--- apps/bot/src/lambda.ts
+++ apps/bot/src/lambda.ts
@@ -1,11 +1,17 @@
+import * as Sentry from '@sentry/aws-serverless';
+
 import { createNestApp } from './app';
 import { TelegramScheduler } from './modules/telegram/telegram.scheduler';
 
-export async function handler() {
+Sentry.init({
+  dsn: 'https://e26c2201258d3b66c33c88251036f008@o4507301057658880.ingest.de.sentry.io/4507553132773456',
+});
+
+export const handler = Sentry.wrapHandler(async () => {
   const app = await createNestApp();
 
   const scheduler = app.get(TelegramScheduler);
 
   await scheduler.execute();
   await app.close();
-}
+});

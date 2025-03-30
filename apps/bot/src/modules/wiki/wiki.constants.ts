e19b703e7231aadc90c73acce93a29c759f00d6c
nonamich <dimaeromin97@gmail.com>
2025-03-30 16:37:36 +0300

    Refactor Wiki module to remove hardcoded base URL; implement dynamic base URL generation based on language.

--- apps/bot/src/modules/wiki/wiki.constants.ts
+++ apps/bot/src/modules/wiki/wiki.constants.ts
@@ -1,6 +1,5 @@
 import { SUPPORTED_LANGUAGES } from '@repo/shared';
 
-export const WIKI_BASE_URL = 'https://api.wikimedia.org/feed/v1/wikipedia';
 export const WIKI_CACHE_ENCODING: BufferEncoding = 'base64';
 export const WIKI_LANGUAGES = SUPPORTED_LANGUAGES;
 export const WIKI_RETRY_MS = 5000;
